08917570a0a2078b5cb3d856e0864c6680f41a46
amanuel <amanuelyihunie27@gmail.com>
2023-04-26 17:47:51 +0300

    [Backend] add timestamp to userschema

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,42 +1,45 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'user name is required'],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'user name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: [true, 'email must be unique'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+    },
+    questions: {
+      type: [{ type: mongoose.Types.ObjectId, ref: 'Question' }],
+      default: [],
+    },
+    answers: {
+      type: [{ type: mongoose.Types.ObjectId, ref: 'Answer' }],
+    },
+    reputation: {
+      type: Number,
+      default: 0,
+    },
+    followers: {
+      type: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+    followings: {
+      type: [{ type: mongoose.Types.ObjectId, ref: 'User', default: [] }],
+    },
+    //   TODO: what is date
   },
-  email: {
-    type: String,
-    required: [true, 'email is required'],
-    unique: [true, 'email must be unique'],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePic: {
-    type: String,
-  },
-  questions: {
-    type: [{ type: mongoose.Types.ObjectId, ref: 'Question' }],
-    default: [],
-  },
-  answers: {
-    type: [{ type: mongoose.Types.ObjectId, ref: 'Answer' }],
-  },
-  reputation: {
-    type: Number,
-    default: 0,
-  },
-  followers: {
-    type: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
-    default: [],
-  },
-  followings: {
-    type: [{ type: mongoose.Types.ObjectId, ref: 'User', default: [] }],
-  },
-  //   TODO: what is date
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
