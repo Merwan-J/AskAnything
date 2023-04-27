d10dc9b09f9db86b494eaa1425276d1534da6898
Merwan-J <merwanj71@gmail.com>
2023-04-27 16:40:58 +0300

    [backend] add jwt authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const app = express();
-
-// Routes
 const userRouter = require('./routes/userRoutes');
+const authRouter = require('./routes/authRoutes');
+const auth = require('./middlewares/auth');
+
+const app = express();
+const prefix = '/api/v1';
 
 app.use(express.json());
 
-app.use('/users', userRouter);
+// for now accessing paths are with no jwt token, if needed just put 'auth' middleware
+app.use(`${prefix}/users`, userRouter);
+app.use(`${prefix}/auth`, authRouter);
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token');
+  }
+});
 
 //404 response
 app.all('*', (req, res) => {
