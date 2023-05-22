e8a5e15e02403d30ca367126fb69e71573c147b8
Elizur <elisurfz@gmail.com>
2023-05-22 16:36:08 +0300

    [Mobile] finished domain layer auth

---
+++ Mobile/lib/domain/auth/auth_service_interface.dart
@@ -0,0 +1,21 @@
+import 'package:askanything/domain/auth/signup_form.dart';
+import 'change_password_form.dart';
+import 'login_form.dart';
+
+abstract class IAuthService{  
+
+  Future<dynamic> signup({required SignUpForm signUpForm});
+
+  Future<dynamic> login({required LoginForm loginForm});
+
+  Future<dynamic> changePassword(
+      {required ChangePasswordForm changePasswordForm});  
+
+  Future<void> logout();
+
+
+}
+
+
+
+
