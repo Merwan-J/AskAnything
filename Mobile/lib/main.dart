ed9206ead3d3ab04a117ec1c36ca26e5cb841c97
Merwan-J <merwanj71@gmail.com>
2023-05-26 03:31:01 +0300

    [Mobile] add Search Bar - Presentation Layer

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:askanything/presentation/base/bottomBar.dart';
 import 'package:askanything/presentation/pages/login_and_registration/login/login_screen.dart';
 import 'package:askanything/presentation/pages/login_and_registration/login/register_screen.dart';
+import 'package:askanything/presentation/pages/search_page/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:askanything/presentation/splash_screen/splash_screen.dart';
 import 'package:askanything/domain/answer/answer.dart';
@@ -20,72 +21,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        builder: (BuildContext context, Widget? child) {
-          return MaterialApp(
-            home: child,
-          );
-        },
-        child: Scaffold(body: Home()));
-  }
-}
-
-class Home extends StatelessWidget {
-  Question question = Question(
-      id: "1",
-      title:
-          "What do you think is the best GPU to play Apex nds?  Do you have any suggestions?",
-      description:
-          "What do you think is the best GPU to play Apex Legends?  Do you have any suggestions?",
-      author: "Merwan Junyedi",
-      anonymous: false,
-      answers: ["1", "2", "3"],
-      topic: "Technology",
-      likes: ["1", "2"],
-      dislikes: ["1,2"],
-      createdAt: DateTime.now(),
-      updatedAt: DateTime.now());
-  Answer answer = Answer(
-      id: "1",
-      text: "I don't know man",
-      image: "",
-      likes: ["1", "3"],
-      dislikes: ["1,23"],
-      author: "Doju",
-      anonymous: false,
-      question: "2",
-      createdAt: DateTime.now(),
-      updatedAt: DateTime.now());
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        builder: (BuildContext context, Widget? child) {
-          return MaterialApp(
-            home: child,
-          );
-        },
-        child: Home());
-  }
-}
-
-class AnotherHome extends StatelessWidget {
-  Question question = Question(
-      id: "1",
-      title:
-          "What do you think is the best GPU to play Apex nds?  Do you have any suggestions?",
-      description:
-          "What do you think is the best GPU to play Apex Legends?  Do you have any suggestions?",
-      author: "Merwan Junyedi",
-      anonymous: false,
-      answers: ["1", "2", "3"],
-      topic: "Technology",
-      likes: ["1", "2"],
-      dislikes: ["1,2"],
-      createdAt: DateTime.now(),
-      updatedAt: DateTime.now());
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: BottomBar());
+    return MaterialApp(
+      home: Scaffold(
+          body: SafeArea(
+        child: SearchScreen(),
+      )),
+    );
   }
 }
