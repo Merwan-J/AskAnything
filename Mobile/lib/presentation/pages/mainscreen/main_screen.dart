45a8dd502c645699dc5a96380e80c4802bcecc7e
abenezer444 <abenezermelaku3@gmail.com>
2023-06-01 19:34:48 +0300

    bookmarks page done

--- Mobile/lib/presentation/pages/mainscreen/main_screen.dart
+++ Mobile/lib/presentation/pages/mainscreen/main_screen.dart
@@ -79,9 +79,6 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   void _onTap(int index) {
-    // if (index == 2) {
-    //   return;
-    // }
     setState(() {
       _selectedIndex = index;
     });
